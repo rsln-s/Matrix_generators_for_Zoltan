d1e2e3e0d0f833c1e6d2a64639e0b2b6c5b3c044
Ruslan Shaydulin <rshaydu@g.clemson.edu>
2017-03-07 17:50:55 -0500

    some fixes and parallelization added

--- hmetis.m
+++ hmetis.m
@@ -1,12 +1,15 @@
-files = {'LPnetlib/lp_ken_07','HB/sherman3'};
+files = {'QLi/largebasis', 'Mittelmann/cont11_l', 'Springer/ESOC', 'JGD_GL7d/GL7d15', 'Chen/pkustk13', 'DIMACS10/auto', 'Bates/sls', 'Mittelmann/cont1_l', 'vanHeukelum/cage13', 'Chen/pkustk12', 'Rucci/Rucci1', 'Meszaros/degme', 'JGD_GL7d/GL7d16', 'JGD_GL7d/GL7d21', 'JGD_Relat/rel9', 'JGD_Relat/relat9'};
+numfiles = numel(files);
 
-for f = files
-    struct = UFget(f{1});
+% UFL1
+% {'QLi/largebasis', 'Mittelmann/cont11_l', 'Springer/ESOC', 'JGD_GL7d/GL7d15', 'Chen/pkustk13', 'DIMACS10/auto', 'Bates/sls', 'Mittelmann/cont1_l', 'vanHeukelum/cage13', 'Chen/pkustk12', 'Rucci/Rucci1', 'Meszaros/degme', 'JGD_GL7d/GL7d16', 'JGD_GL7d/GL7d21', 'JGD_Relat/rel9', 'JGD_Relat/relat9'};
+
+parfor k=1:numfiles
+    f = files{k};
+    struct = UFget(f);
     mat = struct.A;
-    % mat = eye(5);
-    % mat(:,1) = 1;
 
-    fname = strsplit(f{1}, '/');
+    fname = strsplit(f, '/');
     fname = fname{2};
     
     outname = strcat('hmetis_converted/', fname);
