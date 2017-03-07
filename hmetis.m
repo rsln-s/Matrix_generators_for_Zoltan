f1993981003b83cf3f75ac5f57b1120b5c3ce492
Ruslan Shaydulin <rshaydu@g.clemson.edu>
2017-03-07 17:33:18 -0500

    added thing that pulls from UFL and converts to hmetis format

--- hmetis.m
+++ hmetis.m
@@ -1,26 +1,22 @@
-% struct = UFget('HB/bcspwr10');
-% mat = struct.A;
-% mat = eye(5);
-% mat(:,1) = 1;
-vnum = 200;
-enum = 201;
+files = {'LPnetlib/lp_ken_07','HB/sherman3'};
 
-A_left = randi([0 1], ((enum-1)/2), (vnum/2));
-B_right = randi([0,1], ((enum-1)/2), (vnum/2));
-row = zeros(1, vnum);
-row(((vnum/2)-1):((vnum/2)+1)) = 1;
-A_right = zeros(((enum-1)/2),(vnum/2));
-B_left = zeros(((enum-1)/2),(vnum/2));
-A = horzcat(A_left, A_right);
-B = horzcat(B_left, B_right);
-mat = vertcat(A, row, B);
+for f = files
+    struct = UFget(f{1});
+    mat = struct.A;
+    % mat = eye(5);
+    % mat(:,1) = 1;
 
-output = fopen('trivial_two_parts.txt','w+');
-[rows, columns] = size(mat)
+    fname = strsplit(f{1}, '/');
+    fname = fname{2};
+    
+    outname = strcat('hmetis_converted/', fname);
+    output = fopen(outname,'w+');
+    [rows, columns] = size(mat);
 
-%number of hyperedges, vertices 
-fprintf(output, '%d %d\n', rows, columns);
-for row = 1:rows
-       fprintf(output, '%d ', find(mat(row, :)));
-       fprintf(output, '\n');
+    %number of hyperedges, vertices 
+    fprintf(output, '%d %d\n', rows, columns);
+    for row = 1:rows
+           fprintf(output, '%d ', find(mat(row, :)));
+           fprintf(output, '\n');
+    end
 end
