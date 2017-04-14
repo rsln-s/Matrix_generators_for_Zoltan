9db586f7d9894b41bba2053070f491e79106490c
Ruslan Shaydulin <rshaydu@g.clemson.edu>
2017-04-14 14:45:48 -0400

    added a script that takes a network, generates a similar and then outputs communities generated by PLM community detection

---
+++ gen_similar_communities.py
@@ -0,0 +1,31 @@
+from networkit import *
+import sys
+
+multiplier = 2
+
+if len(sys.argv) != 2:
+    print('You have to pass filename to this script')
+    sys.exit(-1)
+
+filename = sys.argv[1]
+
+if len(filename.split('.')) != multiplier:
+    print('TODO fix the script! Cant handle it now')
+    sys.exit(-1)
+
+G = readGraph(filename, Format.EdgeListSpaceOne)
+
+gen = generators.LFRGenerator.fit(G, scale=2)
+
+R = gen.generate()
+
+
+plmPartition = community.PLM(R).run().getPartition()
+
+filename_list = filename.split('.')
+outname = filename_list[0] + '_x' + str(multiplier) + '_communities.txt'
+
+with open(outname, 'w+') as outfile:
+    for i in plmPartition.getSubsetIds():
+        outfile.write(" ".join([str(x) for x in plmPartition.getMembers(i)]) + '\n')
+
