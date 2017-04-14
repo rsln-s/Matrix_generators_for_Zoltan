4c25149d9599296cb2cc32a331f5103955dcfbe2
Ruslan Shaydulin <rshaydu@g.clemson.edu>
2017-04-13 21:41:45 -0400

    added conversion to integers

--- power_law.m
+++ power_law.m
@@ -1,4 +1,5 @@
 alp = 5;
+coeff = 3;
 % p(x)=(alpha-1)*x^(-alpha) 
 
 N=1000;
@@ -7,7 +8,8 @@ pow_law = zeros(1, N);
 
 for i=1:N
     k=rand ;
-    pow_law(1, i) = (1-k)^(1/(-alp+1));
+    pow_law(1, i) = round(coeff * (1-k)^(1/(-alp+1)));
 end
-
+max(pow_law)
+min(pow_law)
 histogram(pow_law)
